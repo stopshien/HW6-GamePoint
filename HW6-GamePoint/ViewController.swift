681e1e83352236ef184a57303b7a51919c65ab57
stopshien <stopforxcode@gmail.com>
2023-03-21 15:05:31 +0800

    Initial Commit

---
+++ HW6-GamePoint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6-GamePoint
+//
+//  Created by 沈庭鋒 on 2023/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
